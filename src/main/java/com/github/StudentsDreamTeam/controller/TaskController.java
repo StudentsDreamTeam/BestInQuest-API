8af7ebcadc268f6a56ebbfadaba723a74fc412d3
uikinion <alessandra.zhosan@gmail.com>
2025-05-03 00:16:13 +0700

    delete и update task работает
    create task как будто где-то циклится, если not null убрать в version_history, то работает, а так бедаа:(

--- src/main/java/com/github/StudentsDreamTeam/controller/TaskController.java
+++ src/main/java/com/github/StudentsDreamTeam/controller/TaskController.java
@@ -16,10 +16,12 @@ public class TaskController {
     private TaskService taskService;
 
     @PostMapping("/create")
-    public  TaskDTO createTask(@RequestParam (value = "userID") Integer userId, @RequestBody TaskDTO task){
-        Task task1 = taskService.createTask(userId, Task.fromDTO(task));
+    public TaskDTO createTask(@RequestParam(value = "authorId") Long authorId,
+                              @RequestParam(value = "executorId") Long executorId,
+                              @RequestBody TaskDTO task) {
+        Task task1 = taskService.createTask(authorId, executorId, Task.fromDTO(task));
         TaskDTO taskDTO = TaskDTO.fromORM(task1);
-        return  taskDTO;
+        return taskDTO;
     }
 
     @GetMapping("/user/{userId}")
