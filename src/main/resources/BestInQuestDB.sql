cdbff35597225e9e94a75965500e9920c2bc85d2
IliaBoyaCF <113082640+IliaBoyaCF@users.noreply.github.com>
2025-05-08 15:40:59 +0700

    Revert "Fix db schema(remove capitalization)."
    This reverts commit 799a3db5dd6dc1f7dfb01baeae65a2b691578646.

--- src/main/resources/BestInQuestDB.sql
+++ src/main/resources/BestInQuestDB.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS "Users" (
     id            serial    PRIMARY KEY,
     name           varchar(255) NOT NULL CHECK (length(name) > 0),
     email         varchar(255) NOT NULL UNIQUE CHECK (email ~* '^[^@]+@[^@]+\.[^@]+$'),
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users (
     streak        bigint    NOT NULL CHECK (streak >= 0)
 );
 
-CREATE TABLE IF NOT EXISTS projects (
+CREATE TABLE IF NOT EXISTS "Projects" (
     id                serial    PRIMARY KEY,
     name          varchar(255) NOT NULL CHECK (length(name) > 0),
     description          varchar(255),
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS projects (
     done          boolean   NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS version_history (
+CREATE TABLE IF NOT EXISTS "Version_history" (
     id                          serial    PRIMARY KEY,
     title                   varchar(255) NOT NULL CHECK (length(title) > 0),
     description                    varchar(255),
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS version_history (
     linked_task_id            bigint    NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS clans (
+CREATE TABLE IF NOT EXISTS "Clans" (
     id            serial    PRIMARY KEY,
     name      varchar(255) NOT NULL UNIQUE CHECK (length(name) > 0),
     leader         bigint    NOT NULL CHECK (leader > 0),
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS clans (
     project        bigint    NOT NULL CHECK (project > 0)
 );
 
-CREATE TABLE IF NOT EXISTS clans_participants (
+CREATE TABLE IF NOT EXISTS "Clans_Participants" (
     id              serial    PRIMARY KEY,
     clan            bigint    NOT NULL CHECK (clan > 0),
     user_id    bigint    NOT NULL CHECK (user_id > 0),
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS clans_participants (
     UNIQUE (clan, user_id)
 );
 
-CREATE TABLE IF NOT EXISTS pvp_competitions (
+CREATE TABLE IF NOT EXISTS "PvP_Competitions" (
     id              serial    PRIMARY KEY,
     player_1         bigint    NOT NULL CHECK (player_1 > 0),
     player_2         bigint    NOT NULL CHECK (player_2 > 0 AND player_2 <> player_1),
@@ -70,7 +70,7 @@ CREATE TABLE IF NOT EXISTS pvp_competitions (
     end_date  timestamptz NOT NULL CHECK (end_date >= start_date)
 );
 
-CREATE TABLE IF NOT EXISTS items (
+CREATE TABLE IF NOT EXISTS "Items" (
     id            serial    PRIMARY KEY,
     name      varchar(255) NOT NULL UNIQUE CHECK (length(name) > 0),
     description      varchar(255),
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS items (
     cost     bigint    NOT NULL CHECK (cost >= 0)
 );
 
-CREATE TABLE IF NOT EXISTS users_inventory (
+CREATE TABLE IF NOT EXISTS "Users_Inventory" (
     id             serial    PRIMARY KEY,
     user_id   bigint    NOT NULL CHECK (user_id > 0),
     item        bigint    NOT NULL CHECK (item > 0),
@@ -90,7 +90,7 @@ CREATE TABLE IF NOT EXISTS users_inventory (
     UNIQUE (user_id, item)
 );
 
-CREATE TABLE IF NOT EXISTS shop (
+CREATE TABLE IF NOT EXISTS "Shop" (
     id            serial    PRIMARY KEY,
     item       bigint    NOT NULL CHECK (item > 0),
     cost          bigint    NOT NULL CHECK (cost >= 0),
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS shop (
     UNIQUE (item)
 );
 
-CREATE TABLE IF NOT EXISTS projects_participants (
+CREATE TABLE IF NOT EXISTS "Projects_participants" (
     id             serial    PRIMARY KEY,
     project         bigint    NOT NULL CHECK (project > 0),
     user_id   bigint    NOT NULL CHECK (user_id > 0),
@@ -107,7 +107,7 @@ CREATE TABLE IF NOT EXISTS projects_participants (
     UNIQUE (project, user_id)
 );
 
-CREATE TABLE IF NOT EXISTS tasks_pointers (
+CREATE TABLE IF NOT EXISTS "Tasks_pointers" (
     id             serial    PRIMARY KEY,
     task         bigint    NOT NULL CHECK (task > 0),
     project         bigint    NOT NULL CHECK (project > 0),
@@ -115,7 +115,7 @@ CREATE TABLE IF NOT EXISTS tasks_pointers (
     UNIQUE (task, project)
 );
 
-CREATE TABLE IF NOT EXISTS commentaries (
+CREATE TABLE IF NOT EXISTS "Commentaries" (
     id             serial    PRIMARY KEY,
     task         bigint    NOT NULL CHECK (task > 0),
     user_id   bigint    NOT NULL CHECK (user_id > 0),
@@ -123,19 +123,19 @@ CREATE TABLE IF NOT EXISTS commentaries (
     creation_date  timestamptz NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS spheres (
+CREATE TABLE IF NOT EXISTS "Spheres" (
     id       serial    PRIMARY KEY,
     name varchar(255) NOT NULL CHECK (length(name) > 0),
     project   bigint    NOT NULL CHECK (project > 0)
 );
 
-CREATE TABLE IF NOT EXISTS tasks_spheres (
+CREATE TABLE IF NOT EXISTS "Tasks_Spheres" (
     task bigint NOT NULL CHECK (task > 0),
     sphere  bigint NOT NULL CHECK (sphere > 0),
     PRIMARY KEY (task, sphere)
 );
 
-CREATE TABLE IF NOT EXISTS achievements (
+CREATE TABLE IF NOT EXISTS "Achievements" (
     id            serial    PRIMARY KEY,
     name      varchar(255) NOT NULL UNIQUE CHECK (length(name) > 0),
     description      varchar(255) NOT NULL CHECK (length(description) > 0),
@@ -143,7 +143,7 @@ CREATE TABLE IF NOT EXISTS achievements (
     icon        varchar(255) NOT NULL CHECK (length(icon) > 0)
 );
 
-CREATE TABLE IF NOT EXISTS users_achievements (
+CREATE TABLE IF NOT EXISTS "Users_Achievements" (
     id               serial    PRIMARY KEY,
     user_id     bigint    NOT NULL CHECK (user_id > 0),
     achievement       bigint    NOT NULL CHECK (achievement > 0),
@@ -151,7 +151,7 @@ CREATE TABLE IF NOT EXISTS users_achievements (
     UNIQUE (user_id, achievement)
 );
 
-CREATE TABLE IF NOT EXISTS income (
+CREATE TABLE IF NOT EXISTS "Income" (
     id            serial    PRIMARY KEY,
     user_id  bigint    NOT NULL CHECK (user_id > 0),
     amount    bigint    NOT NULL CHECK (amount >= 0),
@@ -159,7 +159,7 @@ CREATE TABLE IF NOT EXISTS income (
     description      varchar(255)
 );
 
-CREATE TABLE IF NOT EXISTS spendings (
+CREATE TABLE IF NOT EXISTS "Spendings" (
     id            serial    PRIMARY KEY,
     user_id  bigint    NOT NULL CHECK (user_id > 0),
     amount    bigint    NOT NULL CHECK (amount >= 0),
@@ -167,14 +167,14 @@ CREATE TABLE IF NOT EXISTS spendings (
     description      varchar(255)
 );
 
-CREATE TABLE IF NOT EXISTS teams (
+CREATE TABLE IF NOT EXISTS "Teams" (
     id          serial    PRIMARY KEY,
     project_id  bigint    NOT NULL CHECK (project_id > 0),
     name    varchar(255) NOT NULL CHECK (length(name) > 0),
     description    varchar(255)
 );
 
-CREATE TABLE IF NOT EXISTS teams_participants (
+CREATE TABLE IF NOT EXISTS "Teams_Participants" (
     id               serial    PRIMARY KEY,
     team          bigint    NOT NULL CHECK (team > 0),
     user_id     bigint    NOT NULL CHECK (user_id > 0),
@@ -182,49 +182,66 @@ CREATE TABLE IF NOT EXISTS teams_participants (
     UNIQUE (team, user_id)
 );
 
-CREATE TABLE IF NOT EXISTS project_items (
+CREATE TABLE IF NOT EXISTS "Project_items" (
     id        serial    PRIMARY KEY,
     project    bigint    NOT NULL CHECK (project > 0),
     item   bigint    NOT NULL CHECK (item > 0),
     UNIQUE (project, item)
 );
 
-CREATE TABLE IF NOT EXISTS xp_gains (
+CREATE TABLE IF NOT EXISTS "Xp_gains" (
     id            serial    PRIMARY KEY,
     amount    bigint    NOT NULL CHECK (amount >= 0),
     date          timestamptz NOT NULL,
     user_id  bigint    NOT NULL CHECK (user_id > 0)
 );
 
-ALTER TABLE projects ADD CONSTRAINT project_fk3 FOREIGN KEY (owner) REFERENCES users(id);
-ALTER TABLE version_history ADD CONSTRAINT version_history_fk6 FOREIGN KEY (author) REFERENCES users(id);
-ALTER TABLE version_history ADD CONSTRAINT version_history_fk7 FOREIGN KEY (executor) REFERENCES users(id);
-ALTER TABLE clans ADD CONSTRAINT clans_fk2 FOREIGN KEY (leader) REFERENCES users(id);
-ALTER TABLE clans ADD CONSTRAINT clans_fk5 FOREIGN KEY (project) REFERENCES projects(id);
-ALTER TABLE clans_participants ADD CONSTRAINT clans_participants_fk1 FOREIGN KEY (clan) REFERENCES clans(id);
-ALTER TABLE clans_participants ADD CONSTRAINT clans_participants_fk2 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE pvp_competitions ADD CONSTRAINT pvp_competitions_fk1 FOREIGN KEY (player_1) REFERENCES users(id);
-ALTER TABLE pvp_competitions ADD CONSTRAINT pvp_competitions_fk2 FOREIGN KEY (player_2) REFERENCES users(id);
-ALTER TABLE pvp_competitions ADD CONSTRAINT pvp_competitions_fk4 FOREIGN KEY (winner) REFERENCES users(id);
-ALTER TABLE users_inventory ADD CONSTRAINT users_inventory_fk1 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE users_inventory ADD CONSTRAINT users_inventory_fk2 FOREIGN KEY (item) REFERENCES items(id);
-ALTER TABLE shop ADD CONSTRAINT shop_fk1 FOREIGN KEY (item) REFERENCES items(id);
-ALTER TABLE projects_participants ADD CONSTRAINT projects_participants_fk1 FOREIGN KEY (project) REFERENCES projects(id);
-ALTER TABLE projects_participants ADD CONSTRAINT projects_participants_fk2 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE tasks_pointers ADD CONSTRAINT tasks_pointers_fk1 FOREIGN KEY (task) REFERENCES version_history(id);
-ALTER TABLE tasks_pointers ADD CONSTRAINT tasks_pointers_fk2 FOREIGN KEY (project) REFERENCES projects(id);
-ALTER TABLE commentaries ADD CONSTRAINT commentaries_fk1 FOREIGN KEY (task) REFERENCES tasks_pointers(id);
-ALTER TABLE commentaries ADD CONSTRAINT commentaries_fk2 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE spheres ADD CONSTRAINT spheres_fk2 FOREIGN KEY (project) REFERENCES projects(id);
-ALTER TABLE tasks_spheres ADD CONSTRAINT tasks_spheres_fk0 FOREIGN KEY (task) REFERENCES version_history(id);
-ALTER TABLE tasks_spheres ADD CONSTRAINT tasks_spheres_fk1 FOREIGN KEY (sphere) REFERENCES spheres(id);
-ALTER TABLE users_achievements ADD CONSTRAINT users_achievements_fk1 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE users_achievements ADD CONSTRAINT users_achievements_fk2 FOREIGN KEY (achievement) REFERENCES achievements(id);
-ALTER TABLE income ADD CONSTRAINT income_fk1 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE spendings ADD CONSTRAINT spendings_fk1 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE teams ADD CONSTRAINT teams_fk1 FOREIGN KEY (project_id) REFERENCES projects(id);
-ALTER TABLE teams_participants ADD CONSTRAINT teams_participants_fk1 FOREIGN KEY (team) REFERENCES teams(id);
-ALTER TABLE teams_participants ADD CONSTRAINT teams_participants_fk2 FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE project_items ADD CONSTRAINT project_items_fk1 FOREIGN KEY (project) REFERENCES projects(id);
-ALTER TABLE project_items ADD CONSTRAINT project_items_fk2 FOREIGN KEY (item) REFERENCES items(id);
-ALTER TABLE xp_gains ADD CONSTRAINT xp_gains_fk3 FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+ALTER TABLE "Projects" ADD CONSTRAINT "project_fk3" FOREIGN KEY ("owner") REFERENCES "Users"("id");
+ALTER TABLE "Version_history" ADD CONSTRAINT "Version_history_fk6" FOREIGN KEY ("author") REFERENCES "Users"("id");
+
+ALTER TABLE "Version_history" ADD CONSTRAINT "Version_history_fk7" FOREIGN KEY ("executor") REFERENCES "Users"("id");
+ALTER TABLE "Clans" ADD CONSTRAINT "Clans_fk2" FOREIGN KEY ("leader") REFERENCES "Users"("id");
+
+ALTER TABLE "Clans" ADD CONSTRAINT "Clans_fk5" FOREIGN KEY ("project") REFERENCES "Projects"("id");
+ALTER TABLE "Clans_Participants" ADD CONSTRAINT "Clans_Participants_fk1" FOREIGN KEY ("clan") REFERENCES "Clans"("id");
+
+ALTER TABLE "Clans_Participants" ADD CONSTRAINT "Clans_Participants_fk2" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "PvP_Competitions" ADD CONSTRAINT "PvP_Competitions_fk1" FOREIGN KEY ("player_1") REFERENCES "Users"("id");
+
+ALTER TABLE "PvP_Competitions" ADD CONSTRAINT "PvP_Competitions_fk2" FOREIGN KEY ("player_2") REFERENCES "Users"("id");
+
+ALTER TABLE "PvP_Competitions" ADD CONSTRAINT "PvP_Competitions_fk4" FOREIGN KEY ("winner") REFERENCES "Users"("id");
+
+ALTER TABLE "Users_Inventory" ADD CONSTRAINT "Users_Inventory_fk1" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+
+ALTER TABLE "Users_Inventory" ADD CONSTRAINT "Users_Inventory_fk2" FOREIGN KEY ("item") REFERENCES "Items"("id");
+ALTER TABLE "Shop" ADD CONSTRAINT "Shop_fk1" FOREIGN KEY ("item") REFERENCES "Items"("id");
+ALTER TABLE "Projects_participants" ADD CONSTRAINT "Projects_participants_fk1" FOREIGN KEY ("project") REFERENCES "Projects"("id");
+
+ALTER TABLE "Projects_participants" ADD CONSTRAINT "Projects_participants_fk2" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Tasks_pointers" ADD CONSTRAINT "Tasks_pointers_fk1" FOREIGN KEY ("task") REFERENCES "Version_history"("id");
+
+ALTER TABLE "Tasks_pointers" ADD CONSTRAINT "Tasks_pointers_fk2" FOREIGN KEY ("project") REFERENCES "Projects"("id");
+ALTER TABLE "Commentaries" ADD CONSTRAINT "Commentaries_fk1" FOREIGN KEY ("task") REFERENCES "Tasks_pointers"("id");
+
+ALTER TABLE "Commentaries" ADD CONSTRAINT "Commentaries_fk2" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Spheres" ADD CONSTRAINT "Spheres_fk2" FOREIGN KEY ("project") REFERENCES "Projects"("id");
+ALTER TABLE "Tasks_Spheres" ADD CONSTRAINT "Tasks_Spheres_fk0" FOREIGN KEY ("task") REFERENCES "Version_history"("id");
+
+ALTER TABLE "Tasks_Spheres" ADD CONSTRAINT "Tasks_Spheres_fk1" FOREIGN KEY ("sphere") REFERENCES "Spheres"("id");
+
+ALTER TABLE "Users_Achievements" ADD CONSTRAINT "Users_Achievements_fk1" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+
+ALTER TABLE "Users_Achievements" ADD CONSTRAINT "Users_Achievements_fk2" FOREIGN KEY ("achievement") REFERENCES "Achievements"("id");
+ALTER TABLE "Income" ADD CONSTRAINT "Income_fk1" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Spendings" ADD CONSTRAINT "Spendings_fk1" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Teams" ADD CONSTRAINT "Teams_fk1" FOREIGN KEY ("project_id") REFERENCES "Projects"("id");
+ALTER TABLE "Teams_Participants" ADD CONSTRAINT "Teams_Participants_fk1" FOREIGN KEY ("team") REFERENCES "Teams"("id");
+
+ALTER TABLE "Teams_Participants" ADD CONSTRAINT "Teams_Participants_fk2" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Project_items" ADD CONSTRAINT "Project_items_fk1" FOREIGN KEY ("project") REFERENCES "Projects"("id");
+
+ALTER TABLE "Project_items" ADD CONSTRAINT "Project_items_fk2" FOREIGN KEY ("item") REFERENCES "Items"("id");
+ALTER TABLE "Xp_gains" ADD CONSTRAINT "Xp_gains_fk3" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
