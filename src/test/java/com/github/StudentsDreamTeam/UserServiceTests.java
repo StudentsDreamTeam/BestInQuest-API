6cbf1ff2b0192b5d3634172a3080dec40b2a9d29
IliaBoyaCF <ilia.boya.cf@yandex.ru>
2025-05-27 23:46:07 +0700

    PJKT-79: add more userService tests.

--- src/test/java/com/github/StudentsDreamTeam/UserServiceTests.java
+++ src/test/java/com/github/StudentsDreamTeam/UserServiceTests.java
@@ -1,6 +1,6 @@
 package com.github.StudentsDreamTeam;
 
-import com.github.StudentsDreamTeam.model.LevelRequirement;
+import com.github.StudentsDreamTeam.dto.UpdateUserProfileDTO;
 import com.github.StudentsDreamTeam.model.User;
 import com.github.StudentsDreamTeam.repository.LevelRequirementRepository;
 import com.github.StudentsDreamTeam.repository.UserRepository;
@@ -11,8 +11,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.mockito.Mockito;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserServiceTests {
+
+    private final LocalDateTime now = LocalDateTime.now();
+    private final LocalDateTime yesterday = now.minusDays(1);
+    private final LocalDateTime longTimeAgo = now.minusYears(2).minusDays(5).minusMonths(1);
+
     @Test
     @DisplayName("Normal registration")
     public void normalRegistration() {
@@ -20,8 +26,6 @@ public class UserServiceTests {
         LevelRequirementRepository mockLevelRequirementRepository = Mockito.mock(LevelRequirementRepository.class);
 
         UserService userService = new UserService(mockUserRepository, mockLevelRequirementRepository);
-
-        LocalDateTime now = LocalDateTime.now();
 
         User expected = new User();
         expected.setName("Juan");
@@ -61,8 +65,6 @@ public class UserServiceTests {
 
         UserService userService = new UserService(mockUserRepository, mockLevelRequirementRepository);
 
-        LocalDateTime now = LocalDateTime.now();
-
         User expected = new User();
         expected.setName("Juan");
         expected.setXp(120);
@@ -80,5 +82,151 @@ public class UserServiceTests {
 
         Mockito.verify(mockUserRepository).existsByEmail(expected.getEmail());
         Mockito.verifyNoMoreInteractions(mockUserRepository);
+    }
+
+    @Test
+    @DisplayName("Normal flow of changing user profile info")
+    public void updateProfileInfo() {
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        LevelRequirementRepository mockLevelRequirementRepository = Mockito.mock(LevelRequirementRepository.class);
+
+        UserService userService = new UserService(mockUserRepository, mockLevelRequirementRepository);
+
+        User old = new User();
+        old.setName("Juan");
+        old.setXp(120);
+        old.setLevel(1);
+        old.setLastInDate(now);
+        old.setRegistrationDate(now);
+        old.setEmail("example@example.org");
+        old.setPassword("some strong password");
+        old.setStreak(0);
+
+        User expected = new User();
+        expected.setName("Quixote");
+        expected.setXp(120);
+        expected.setLevel(1);
+        expected.setLastInDate(now);
+        expected.setRegistrationDate(now);
+        expected.setEmail("changed@example.org");
+        expected.setPassword("different password");
+        expected.setStreak(0);
+
+        Mockito.when(mockUserRepository.findById(0L)).thenReturn(Optional.of(old));
+        Mockito.when(mockUserRepository.save(expected)).thenReturn(expected);
+        Mockito.when(mockUserRepository.existsByEmail(old.getEmail())).thenReturn(true);
+        Mockito.when(mockUserRepository.existsByEmail(expected.getEmail())).thenReturn(false);
+
+        User actual = userService.updateUserProfile(0,
+                new UpdateUserProfileDTO("Quixote", "changed@example.org", "different password"));
+
+        Mockito.verify(mockUserRepository).findById(0L);
+        Mockito.verify(mockUserRepository).existsByEmail(expected.getEmail());
+        Mockito.verify(mockUserRepository).save(expected);
+
+        Assertions.assertEquals(expected.getName(), actual.getName());
+        Assertions.assertEquals(expected.getXp(), actual.getXp());
+        Assertions.assertEquals(expected.getLevel(), actual.getLevel());
+        Assertions.assertEquals(expected.getLastInDate(), actual.getLastInDate());
+        Assertions.assertEquals(expected.getRegistrationDate(), actual.getRegistrationDate());
+        Assertions.assertEquals(expected.getEmail(), actual.getEmail());
+        Assertions.assertEquals(expected.getPassword(), actual.getPassword());
+        Assertions.assertEquals(expected.getStreak(), actual.getStreak());
+
+    }
+
+    @Test
+    @DisplayName("Setting email that already exists")
+    public void invalidUpdateProfileInfo() {
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        LevelRequirementRepository mockLevelRequirementRepository = Mockito.mock(LevelRequirementRepository.class);
+
+        UserService userService = new UserService(mockUserRepository, mockLevelRequirementRepository);
+
+        User old = new User();
+        old.setName("Juan");
+        old.setXp(120);
+        old.setLevel(1);
+        old.setLastInDate(now);
+        old.setRegistrationDate(now);
+        old.setEmail("example@example.org");
+        old.setPassword("some strong password");
+        old.setStreak(0);
+
+        User expected = new User();
+        expected.setName("Quixote");
+        expected.setXp(120);
+        expected.setLevel(1);
+        expected.setLastInDate(now);
+        expected.setRegistrationDate(now);
+        expected.setEmail("changed@example.org");
+        expected.setPassword("different password");
+        expected.setStreak(0);
+
+        Mockito.when(mockUserRepository.findById(0L)).thenReturn(Optional.of(old));
+        Mockito.when(mockUserRepository.save(expected)).thenReturn(expected);
+        Mockito.when(mockUserRepository.existsByEmail(old.getEmail())).thenReturn(true);
+        Mockito.when(mockUserRepository.existsByEmail(expected.getEmail())).thenReturn(true);
+
+        Assertions.assertThrows(IllegalStateException.class, () -> userService.updateUserProfile(0,
+                new UpdateUserProfileDTO("Quixote", "changed@example.org", "different password")));
+
+        Mockito.verify(mockUserRepository).findById(0L);
+        Mockito.verify(mockUserRepository).existsByEmail(expected.getEmail());
+        Mockito.verifyNoInteractions(mockUserRepository);
+
+    }
+
+    @Test
+    @DisplayName("Checks if streak updates")
+    public void checkStreakUpdate() {
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        LevelRequirementRepository mockLevelRequirementRepository = Mockito.mock(LevelRequirementRepository.class);
+
+        UserService userService = new UserService(mockUserRepository, mockLevelRequirementRepository);
+
+        User old = new User();
+        old.setName("Juan");
+        old.setXp(120);
+        old.setLevel(1);
+        old.setLastInDate(yesterday);
+        old.setRegistrationDate(longTimeAgo);
+        old.setEmail("example@example.org");
+        old.setPassword("some strong password");
+        old.setStreak(10);
+
+        User expected = new User();
+        expected.setName("Juan");
+        expected.setXp(120);
+        expected.setLevel(1);
+        expected.setRegistrationDate(longTimeAgo);
+        expected.setEmail("example@example.org");
+        expected.setPassword("some strong password");
+        expected.setStreak(11);
+
+        Mockito.when(mockUserRepository.findById(0L)).thenReturn(Optional.of(old));
+        Mockito.when(mockUserRepository.save(expected)).thenReturn(expected);
+
+        User actual = userService.getUserProfile(0L);
+
+        Mockito.verify(mockUserRepository).findById(0L);
+        Mockito.verify(mockUserRepository).save(Mockito.argThat(
+                argument -> expected.getName().equals(argument.getName()) &&
+                                expected.getXp().equals(argument.getXp()) &&
+                                expected.getLevel().equals(argument.getLevel()) &&
+                                expected.getRegistrationDate().equals(argument.getRegistrationDate()) &&
+                                expected.getEmail().equals(argument.getEmail()) &&
+                                expected.getPassword().equals(argument.getPassword()) &&
+                                expected.getStreak().equals(argument.getStreak())));
+
+        Assertions.assertEquals(expected.getName(), actual.getName());
+        Assertions.assertEquals(expected.getXp(), actual.getXp());
+        Assertions.assertEquals(expected.getLevel(), actual.getLevel());
+        Assertions.assertTrue(expected.getLastInDate().isAfter(actual.getLastInDate()));
+        Assertions.assertEquals(expected.getRegistrationDate(), actual.getRegistrationDate());
+        Assertions.assertEquals(expected.getEmail(), actual.getEmail());
+        Assertions.assertEquals(expected.getPassword(), actual.getPassword());
+        Assertions.assertEquals(expected.getStreak(), actual.getStreak());
+
     }
 }
